83ac48909387f6ba3d47da236dd1a9e00b238dba
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-09-17 18:27:06 -0400

    fixed burgers.js file

--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -1,31 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
         event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#ca").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                location.reload();
+            }
+        );
+    });
+
+    $(".devour-btn").on("click", function () {
+        var id = $(this).attr("data-burger");
+        var devBurger = {
+            devoured: 1
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devBurger
+        }).then(
+            function () {
+                console.log("you ate the burger!");
+                location.reload();
+            }
+        );
     })
-})
-
-var newBurger = {
-    burger_name: $("#ca").val().trim(),
-    devoured: 0
-
-};
-    $.ajax("/api/burgers", {
-    type: "POST",
-    data: newBurger, 
-}).then(
-    function () {
-        console.log("created new burger");
-        location.reload();
-    }
-)
-
-
-$(".devour-btn").on("click", function () {
-    var id = $(this).attr("data-burger")
-    console.log(id); 
-   $.ajax
-})
-
-
-// Export the database functions for the controller (catsController.js).
-module.exports = burger;
+});
