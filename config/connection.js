06f06e955f224896708e8bf2fee53a4c1d627695
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-09-17 19:50:30 -0400

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -16,11 +16,11 @@ var connection = mysql.createConnection({
 // Make connection.
 connection.connect();
 
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+  // if (err) {
+  //   console.error("error connecting: " + err.stack);
+  //   return;
+  // }
+  // console.log("connected as id " + connection.threadId);
 
 // Export connection for our ORM to use.
 module.exports = connection;
